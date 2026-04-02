992ec484bf23bb58c3d397d8905497abd89ff420
Saket234648 <barnwalsaket73@gmail.com>
2026-04-02 17:47:04 +0530

    fix api base url

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://daily-kharcha-tracker.onrender.com/',
+  baseURL: 'https://daily-kharcha-tracker.onrender.com/api',
 });
 
 // Add token to requests if available
